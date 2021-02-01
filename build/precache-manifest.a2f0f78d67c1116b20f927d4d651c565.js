93d3edde8751802f3bd1a474308b58f15ad81d4e
Lucian Perniciaro <perniciaro.lucian@tamu.edu>
2021-01-31 22:59:31 -0600

    test2

---
+++ build/precache-manifest.a2f0f78d67c1116b20f927d4d651c565.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c89046efc19f6bcfbdde",
+    "url": "/static/js/runtime~main.3511b81a.js"
+  },
+  {
+    "revision": "0e6ca9c794e8a0a61229",
+    "url": "/static/js/main.7a2b9674.chunk.js"
+  },
+  {
+    "revision": "cea2cb00e4c73111dfd8",
+    "url": "/static/js/3.429d8ae9.chunk.js"
+  },
+  {
+    "revision": "eede3b9c4962d81af586",
+    "url": "/static/js/2.98051120.chunk.js"
+  },
+  {
+    "revision": "0e6ca9c794e8a0a61229",
+    "url": "/static/css/main.97ee6477.chunk.css"
+  },
+  {
+    "revision": "d2d4014dd511f5f1ea349f0377b3dafb",
+    "url": "/index.html"
+  }
+];
