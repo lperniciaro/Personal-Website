8bbbe4454bf75a127cb905eb7aaba4f8d9085b70
lperniciaro <e>
2024-11-29 22:45:50 -0800

    update aboutme

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,12 +11,12 @@ export default function AboutMe() {
             <GoToTop />
             <div>
                 <h1 className='about-me'>ABOUT ME</h1>
-                <div className='smallcard'>I was born in New Orleans, Louisiana. I moved shortly after to Texas due to hurricane Katrina. As I grew up I had
-                    an interest in wanting to understand how "things" work. Not too long into highschool I decided I wanted to become an engineer.
+                <div className='smallcard'>I was born in New Orleans, Louisiana. I moved shortly after to Texas due to hurricane Katrina. After graduation, I moved
+                    to the lovely city of San Diego, CA. 
                 </div>
                 
                 <div className='smallcard'>
-                        I chose Computer Engineering as my degree path, as it gives me both a taste of hardware and software development. I enjoy software development 
+                        I gradauted in Computer Engineering. I chose this degree as it gave me both a taste of hardware and software development. I enjoy software development 
                         as it gives me a chance to create anything to my imagination. Thinking out of the box and creating a solution to a problem is one of my favorite
                         things to do, and software engineering allows me to accomplish that in many ways.
                 </div>
@@ -27,9 +27,9 @@ export default function AboutMe() {
                 <div className='smallcard'>
                     <div className='leftdiv'>
 
-                        Outside of engineering I like to apply my problem solving towards rallycross. Rallycross is a time trial dirt racing series 
+                        Outside of engineering I like to apply my problem solving towards cars. I participate in Rallycross, a time trial dirt racing series 
                         which emphasises motorsport competition on a budget. I bought a Mercury Grand Marquis off of craigslist for $600. Spent some
-                        time tinkering, playing mechanic to get it in running condition, and have been enjoying the sport ever since. I bought
+                        time tinkering to get it in running condition, and have been enjoying it ever since. I bought
                         the car solely for racing, but it has turned out to be an extremely reliable vehicle. I daily drive it to this day. Even with all the abuse
                         I put it through it still holds up as a great car with no issues.
                         I rallycross at <a href="https://www.lonestarrallycross.com/" className='a'>LoneStar Rallycross</a>
